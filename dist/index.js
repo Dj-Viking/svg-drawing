e0feb4c88fd08d21f8a94305b7a08d7f4a3a8bed
dj-viking <anders.swedishviking@gmail.com>
2022-12-31 20:02:45 -0500

    can draw svgs, and refactoring

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+"use strict";
+/// <reference types="." />
+class Main {
+    constructor() {
+        this._paths = document.querySelectorAll("svg path");
+        this._drawAlongPaths();
+    }
+    _drawAlongPaths() {
+        this._paths.forEach((path, index) => {
+            const pathLength = path.getTotalLength();
+            path.setAttribute("stroke-dasharray", pathLength.toString());
+            path.setAttribute("stroke-dashoffset", pathLength.toString() + 1);
+            path.innerHTML = this._createAnimationTag(pathLength, index);
+            console.log("path info", index, pathLength, path.innerHTML);
+        });
+    }
+    _createBeginTime(index) {
+        if (index === 0) {
+            return "0s";
+        }
+        else {
+            return `a${index - 1}.end`;
+        }
+    }
+    _createAnimationTagId(index) {
+        if (index === 0) {
+            return 0;
+        }
+        else {
+            return index;
+        }
+    }
+    _createAnimationTag(pathLength, index) {
+        //some relative number to calculate how fast a path will draw itself based on how long the path is
+        const speed = 100;
+        //dynamically create the animation tag based on the order that the paths are found in the path node list
+        // first one is assigned id 0 as first index..next tag is assigned id of next index
+        //animation of next path starts when the previous animation tag ends
+        return `
+            <animate
+                id=a${this._createAnimationTagId(index)}
+                attributeName='stroke-dashoffset'
+                begin='${this._createBeginTime(index)}'
+                dur='${pathLength / speed}s'
+                from='${pathLength}'
+                to='0'
+                fill='freeze'
+            />
+        `;
+    }
+}
---
+++ index.ts
@@ -0,0 +1,61 @@
+/// <reference types="." />
+
+class Main {
+    private _paths: NodeListOf<SVGPathElement>;
+
+    constructor() {
+        this._paths = document.querySelectorAll<SVGPathElement>("svg path");
+        this._drawAlongPaths();
+    }
+
+    private _drawAlongPaths(): void {
+        this._paths.forEach((path, index) => {
+            const pathLength = path.getTotalLength();
+
+            path.setAttribute("stroke-dasharray", pathLength.toString());
+            path.setAttribute("stroke-dashoffset", pathLength.toString() + 1);
+
+            path.innerHTML = this._createAnimationTag(pathLength, index);
+
+            console.log("path info", index, pathLength, path.innerHTML);
+        });
+    }
+
+    private _createBeginTime(index: number): string {
+        if (index === 0) {
+            return "0s";
+        } else {
+            return `a${index - 1}.end`;
+        }
+    }
+
+    private _createAnimationTagId(index: number): number {
+        if (index === 0) {
+            return 0;
+        } else {
+            return index;
+        }
+    }
+
+    private _createAnimationTag(pathLength: number, index: number): string {
+        //some relative number to calculate how fast a path will draw itself based on how long the path is
+        const speed = 100;
+
+        //dynamically create the animation tag based on the order that the paths are found in the path node list
+        // first one is assigned id 0 as first index..next tag is assigned id of next index
+
+        //animation of next path starts when the previous animation tag ends
+
+        return `
+            <animate
+                id=a${this._createAnimationTagId(index)}
+                attributeName='stroke-dashoffset'
+                begin='${this._createBeginTime(index)}'
+                dur='${pathLength / speed}s'
+                from='${pathLength}'
+                to='0'
+                fill='freeze'
+            />
+        `;
+    }
+}
